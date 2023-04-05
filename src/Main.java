69ba3315b4d6580a128f1c18c7132f426862cf5f
vtp <vtp@ordix.de>
2023-04-05 13:09:51 +0200

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Haus haus1 = new Haus();
-        haus1.setHoehe(5f);
-        System.out.println("Die Haushoehe ist "+haus1.getHoehe());
+        haus1.setHoehe(-3f);
+        //System.out.println("Die Haushoehe ist "+haus1.getHoehe());
     }
 }
